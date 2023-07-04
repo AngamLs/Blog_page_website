e139e9b5d7f56951761ebfd1e940520b77a03ce8
Leo Shimray <rexmccartney@gmail.com>
2023-07-04 16:54:13 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,70 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent =
+  "This is a simple blog posting page created using express.js . Each blog page you create has it's own stand alone page. Press the compose button below to try it out. You can click the 'Read more...' to get directed to the stand alone page. Enjoy ✌️";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let posts = [];
+
+app.get("/", (req, res) => {
+  res.render("home", { 
+    homeStartingContent: homeStartingContent,
+    posts: posts
+   });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", (req, res) => { 
+  res.render("compose");
+});
+
+app.get("/post/:postName", (req, res) => {
+   const requestedTitle = _.lowerCase(req.params.postName); // we are using the lodash module to ignore uppercase and spaces in the url.
+
+  for(let post of posts) {
+    const storedTitle = _.lowerCase(post.title); // using the lodash module again.
+    if(storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
+  }
+});
+
+
+
+app.post("/compose", (req,res) => {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
+})
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
